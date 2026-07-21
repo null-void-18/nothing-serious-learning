562d7696ef966148a23f6830db1a1fc57c691c66
Kiran Sekhar <kiransekhar18@gmail.com>
2026-07-21 11:02:05 +0530

    practice

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,8 @@
 public class Calculator {
     private static void main(String args[]) {
         int arr[] = new int[7];
+
+        //non sense comments for practising, thank you
+        //FIFA 2026 winner Spain
     }
 }
